59ce2217151b932ffd13a386783e5bc843e9a1a1
AngieMiv FP <angieibarrola@gmail.com>
2025-03-26 19:19:10 +0100

    the 2 projects

--- Aparcamiento-Maven-DB/src/main/java/org/example/DaoList.java
+++ Aparcamiento-Maven-DB/src/main/java/org/example/DaoList.java
@@ -1,16 +1,17 @@
 package org.example;
-
 import java.util.ArrayList;
 
 // Interfaz DaoList, en esta interfaz haremos los esquemas de las funciones que puede implementar otra clase
+
+//T: Type parameter, indicates that that is ANY TYPE
 
 public interface DaoList<T> {
 
 		public ArrayList<T> findAll();
 		public T findOne(String key);
-		public boolean insertOne(T t);
+		public boolean insertOne(T t); 
 		public boolean deleteOne(String key);
 		public boolean updateOne(String key,T t);
 		
 	
-	} // interface DaoList
+} // interface DaoList
