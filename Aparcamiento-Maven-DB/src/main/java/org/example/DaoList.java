d78e98fcadfaebbe0c20f7511bcf553afdfeab16
AngieMiv FP <angieibarrola@gmail.com>
2025-03-26 20:35:07 +0100

    Starting JavaDoc from DaoList

--- Aparcamiento-Maven-DB/src/main/java/org/example/DaoList.java
+++ Aparcamiento-Maven-DB/src/main/java/org/example/DaoList.java
@@ -1,17 +1,34 @@
 package org.example;
 import java.util.ArrayList;
 
-// Interfaz DaoList, en esta interfaz haremos los esquemas de las funciones que puede implementar otra clase
+/**
+ * Defines a contract for data acces operations on a collections of objects of type{@code T}.
+ * 
+ * <p>An interface in Java specifies a set of methods that implementing classes must define,
+ * ensuring a consisten API while allowing flexibiliti in the actual implementation.
+ * This interface follows de DAO (Data Access Object) pattern, providing basic CRUD operations.
+ * 
+ * @param <T> the type of objects managed by this DAO
 
-//T: Type parameter, indicates that that is ANY TYPE
+ * */
+// Interfaz DaoList, esta interfaz hace esquemas de las funciones que pueden implementar otras clases
+//T: Type parameter, indica que es de cualquier tipo, ANY TYPE
 
 public interface DaoList<T> {
-
-		public ArrayList<T> findAll();
-		public T findOne(String key);
-		public boolean insertOne(T t); 
-		public boolean deleteOne(String key);
-		public boolean updateOne(String key,T t);
-		
 	
+	/**
+	 * Retrieves all elements from the data source.
+	 * @return an {@code ArrayList} containing all objects of type {@code T}.
+	 */
+	public ArrayList<T> findAll();
+	
+	
+	public T findOne(String key);
+	
+	public boolean insertOne(T t); 
+	
+	public boolean deleteOne(String key);
+	
+	public boolean updateOne(String key,T t);
+		
 } // interface DaoList
